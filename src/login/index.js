596358e93cc67de60192cb10056b8f7dc3506049
liuguangdong888 <3193735742@qq.com>
2020-08-19 23:41:28 +0800

    增加数据

--- src/login/index.js
+++ src/login/index.js
@@ -3,7 +3,8 @@ import {BrowserRouter} from 'react-router-dom'
 class Login extends React.Component{
     state={
         arr:[{name:"刘冬",age:24,address:"河南罗山"},{name:"刘国庆",age:22,address:"河南罗山"},{name:"刘光绪",age:10,address:"河南罗山"}],
-        num:10
+        num:10,
+        store:100
     }
     fn=()=>{
         return this.state.arr.map((item,index)=>{
@@ -11,7 +12,7 @@ class Login extends React.Component{
                 <ul key={index}>
                     <li >{item.name}</li>
                     <li >{item.age}</li>
-                    <li >{item.address}</li>
+                    <li >{item.address}</li>                   
                 </ul>
             )
         })
