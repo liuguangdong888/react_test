c98c117f0b6542ff2e04874edaa42429fd89c2de
liuguangdong888 <3193735742@qq.com>
2020-08-19 23:05:32 +0800

    第一次修改

--- src/login/index.js
+++ src/login/index.js
@@ -6,7 +6,7 @@ class Login extends React.Component{
         arr:[{name:"刘冬",age:24,address:"河南罗山"},{name:"刘国庆",age:22,address:"河南罗山"},{name:"刘光绪",age:10,address:"河南罗山"}]
     }
     fn=()=>{
-        return this.state.arr.map((item,index)=>{
+        return this.state.arr.map((item,index,elment)=>{
             return (
                 <ul key={index}>
                     <li >{item.name}</li>
