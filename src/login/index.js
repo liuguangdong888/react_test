683b998eded9005054f6d46b1b11df816f0db656
liuguangdong888 <3193735742@qq.com>
2020-08-19 22:58:54 +0800

    登录页面

---
+++ src/login/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import {BrowserRouter} from 'react-router-dom'
+import {Button} from 'antd-mobile'
+class Login extends React.Component{
+    state={
+        arr:[{name:"刘冬",age:24,address:"河南罗山"},{name:"刘国庆",age:22,address:"河南罗山"},{name:"刘光绪",age:10,address:"河南罗山"}]
+    }
+    fn=()=>{
+        return this.state.arr.map((item,index)=>{
+            return (
+                <ul key={index}>
+                    <li >{item.name}</li>
+                    <li >{item.age}</li>
+                    <li >{item.address}</li>
+                </ul>
+            )
+        })
+    }
+    render(){
+        return(
+        <BrowserRouter>
+            <div>
+                 {this.fn()}
+            </div>
+        </BrowserRouter>
+        )
+    }
+}
+export default Login
